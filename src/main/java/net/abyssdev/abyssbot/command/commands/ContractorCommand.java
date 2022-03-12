97123c0273db4aee446e9b459f380dfe14f76732
RelocationDev <g.delgado2006@gmail.com>
2022-03-12 02:11:02 -0500

    balls

--- src/main/java/net/abyssdev/abyssbot/command/commands/ContractorCommand.java
+++ src/main/java/net/abyssdev/abyssbot/command/commands/ContractorCommand.java
@@ -47,14 +47,13 @@ public final class ContractorCommand extends Command {
         final String role = args[1].getAsString();
         final User user = args[0].getAsUser();
 
-        event.getGuild().createTextChannel(user.getName() + "-" + role, event.getGuild().getCategoryById(951919951885795378L)).queue(textChannel -> {
+        event.getGuild().createTextChannel(role + "-" + user.getName(), event.getGuild().getCategoryById(951919951885795378L)).queue(textChannel -> {
             textChannel.createPermissionOverride(event.getGuild().getMember(user)).setAllow(Permission.VIEW_CHANNEL, Permission.MESSAGE_WRITE).queue();
             textChannel.sendMessage(user.getAsMention()).queue();
             textChannel.sendMessage(new EmbedBuilder()
                     .setTitle("AbyssDev | Contractor Channel")
                     .setDescription("Welcome to your contractor channel, " + user.getName()
                             + ".\n\nThis is where all of your AbyssDev related work will be handled." +
-                            "Firstly so we can communicate without issues, and secondly so everyone is filled in." +
                             "\n\n*Please look over the Onboard Document for more information: https://bit.ly/3tSlYc6*" +
                             "\n\n*(( If you have any questions feel free to ping management. ))*")
                     .build()).queue();
